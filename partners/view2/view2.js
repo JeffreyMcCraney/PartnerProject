6a5d80c6437972199e8d6c263b74e8b980b1f150
JeffreyMcCraney <mccraney.j@gmail.com>
2016-03-14 11:21:17 -0600

    updated with legal approval

--- partners/view2/view2.js
+++ partners/view2/view2.js
@@ -10,12 +10,12 @@ angular.module('myApp.view2', ['ngRoute'])
 }])
 
 .controller('View2Ctrl', ['$scope', '$routeParams', '$firebaseObject','$window',function($scope, $routeParams, $firebaseObject, $window) {
-	
+
   console.log($routeParams.company);
 
   $scope.url = $routeParams.company;
 
-	var ref = new Firebase("https://gosolarperks.firebaseio.com/urls/" + $scope.url);
+	var ref = new Firebase("https://solarpartners.firebaseio.com/urls/" + $scope.url);
 
   $scope.data = $firebaseObject(ref);
 
@@ -24,10 +24,20 @@ angular.module('myApp.view2', ['ngRoute'])
     console.log($scope.data);
     if($scope.data.url == $scope.url){
       console.log("true");
+      console.log($scope.data.boxBulletPoint3);
+      if ($scope.data.boxBulletPoint3 != "N/A"){
+        $scope.bulletPoint = true;
+        console.log("boxBullet = true");
+      } else{
+        $scope.bulletPoint = false;
+        console.log("boxBullet = false");
+      }
     }else{
       $window.location.href = '#/home';
     }
   });
+
+  
 
 
 
